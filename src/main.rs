37a1727e679cd535d1deea901fc6e99bcc25574f
Rohan Choudhary <rohan.c2002@gmail.com>
2025-08-07 08:31:04 +0000

    added command line args

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
+use clap::Parser;
+#[derive(Parser)]
+#[command(name = "http")]
+#[command(about = "A simple HTTP client")]
+struct Args {
+    url: String,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>>{
-    let url = "https://httpbin.org/get";
+    let args = Args::parse();
     let client = reqwest::Client::new();
-    let response = client.get(url).send().await?;
+    println!("Making request to: {}", args.url);
+    let response = client.get(&args.url).send().await?;
     println!("Status: {}", response.status());
 
     let body = response.text().await?;
